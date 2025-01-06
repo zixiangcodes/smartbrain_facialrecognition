bd0c01d3edb47c46d4ac7b5233cab1841b94ffe6
Zi Xiang <zixiang150193@hotmail.com>
2025-01-06 17:46:27 +0800

    Allow home page to be accessed for testing purposes.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -13,7 +13,7 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
 			<nav className="navigation">
 				<p onClick={() => onRouteChange('SignIn')} className="nav-link"> Sign In </p>
 				<p onClick={() => onRouteChange('Register')} className="nav-link"> Register </p>
-				{/* <p onClick={() => onRouteChange('Home')} className="nav-link"> Home </p> */}
+				<p onClick={() => onRouteChange('Home')} className="nav-link"> Home </p>
 				{/* To remove ^^ this direct access link once app has been completed. To access home page */}
 			</nav>
 		);
